fb2693126e32f8d40144fcf893f18b4ac4a5a245
zekeyadhurley <hurleyzd@vcu.edu>
2021-12-17 13:20:45 -0500

    Update MainActivity.java

--- app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
+++ app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         if (!checkPermissions()) {
-            //requestPermissions();
+            requestPermissions();
         } else {
             getLastLocation();
         }
